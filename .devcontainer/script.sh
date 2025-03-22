09c0ccc3a3a81b48e8b34acdd25be94a41952df8
rafael barbosa <macbook@MacBook-Pro-de-Rafael.local>
2025-03-22 09:24:23 -0300

    update path on devcontainer

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -3,4 +3,4 @@ set -eux
 sudo apt-get update -y 
 npm i serverless@3.39.0 -g
 mkdir -p ~/.aws/
-cp /workspaces/fiap-cloud-computing-tutorials/.devcontainer/config ~/.aws/config
+cp /workspaces/fiap-arquitetura-compute-e-storage/.devcontainer/config ~/.aws/config
